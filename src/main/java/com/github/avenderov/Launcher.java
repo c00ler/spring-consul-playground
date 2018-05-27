6bb3730d0b438debc3fd5a8bff7540343859dddf
Alexey Venderov <alexey.venderov@n26.com>
2018-05-27 13:50:04 +0200

    first commit

---
+++ src/main/java/com/github/avenderov/Launcher.java
@@ -0,0 +1,15 @@
+package com.github.avenderov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+
+}
